203b9cfe0a3cbdefec8d199431a744bfc97a822a
Eyas Sharary <eyas.wt@gmail.com>
2025-01-30 06:03:29 +0200

    Removed the react (samer's client)

--- client/src/Components/Sockets/SignalR.js
+++
@@ -1,9 +0,0 @@
-import * as signalR from '@microsoft/signalr';
-
-const hubConnection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:5256/hub") // Replace with your server's SignalR Hub URL
-    .withAutomaticReconnect()
-    .build();
-
-
-export default hubConnection;

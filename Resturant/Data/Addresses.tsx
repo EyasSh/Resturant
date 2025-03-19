57ba84d88b2f7e654e1844080e5f6d90337d7000
JulianT2004 <totryjulian@gmail.com>
2025-03-19 16:40:02 +0200

    add the IP for nitc

--- Resturant/Data/Addresses.tsx
+++ Resturant/Data/Addresses.tsx
@@ -1,6 +1,6 @@
 const ip={
     eyas:"10.0.0.162",
     julian:"10.0.0.162", //Continue last digit here
-    nitc:"10.0.0." //Continue last digit here too
+    nitc:"192.168.122.76" //Continue last digit here too
 }
 export default ip

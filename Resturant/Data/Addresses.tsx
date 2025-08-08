fa3f0e433c0205e5f3aa41968a357a4fea2322f7
Eyas Sharary <eyas.wt@gmail.com>
2025-08-08 14:17:30 +0300

    Edited ShowMessegeOnPlat

--- Resturant/Data/Addresses.tsx
+++ Resturant/Data/Addresses.tsx
@@ -1,5 +1,5 @@
 const ip={
-    julian:"10.0.0.39".trim(), //Continue last digit here
+    julian:"10.0.0.160".trim(), //Continue last digit here
     nitc:"192.168.122.19" //Continue last digit here too
 }
 export default ip
--- Resturant/components/ui/ShowMessageOnPlat.tsx
+++ Resturant/components/ui/ShowMessageOnPlat.tsx
@@ -11,8 +11,8 @@ import { Platform, ToastAndroid } from "react-native";
 export default function ShowMessageOnPlat(message: string) {
     if (Platform.OS === "android") {
         ToastAndroid.show(message, ToastAndroid.CENTER);
-    } else if (Platform.OS === "ios") {
-        // Handle iOS-specific logic here if needed
-        alert(message); // For example, you can use alert for iOS
+    } else {
+        // Alert for other platforms (e.g., iOS)
+        alert(message);
     }
 }
